d3f4a9a040479aef6a231495497626ac66a5dd6b
Nishendra Chaudhari <149066940+nishendrachaudhari111@users.noreply.github.com>
2024-01-13 14:58:14 +0530

    dataTypes

---
+++ datatypes.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7 Types : String, Number, Boolean, Null, Undefined, Symbol, BigInt.
+
+const score = 100
+const scoreValue = 100.2
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('1234')
+const anotherId = Symbol('1234')
+
+// console.log(id === anotherId);
+
+//const bigNumber = 34567889345678934567n
+
+
+// Reference (Non primitive)
+
+//Array, Objects, Functions
+
+const heros = ["Shiva", "Zaid khan", "Abhay" ]
+let Myobj = {
+    name: "Nishendar",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof outsideTemp);
+
