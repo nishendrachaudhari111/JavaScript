41aa128e675505cabe12ff770f0e3c1c817d1ce7
Nishendra Chaudhari <149066940+nishendrachaudhari111@users.noreply.github.com>
2024-01-13 13:23:54 +0530

    Conversion of Operation

---
+++ ConversionOperation.js
@@ -0,0 +1,27 @@
+let score = "Abhay"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score) 
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" =>  33
+// "33asd" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Nishendar"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => True; 0 => false
+// "" => false
+// "Nishendar" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
