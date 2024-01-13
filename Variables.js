765b2067469291cec48abf635f1fe17907cd0d81
Nishendra Chaudhari <149066940+nishendrachaudhari111@users.noreply.github.com>
2024-01-13 12:57:31 +0530

    Data Types

---
+++ Variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail ="nc123@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ayodhya"
+let accountState;
+
+//accountId = 1 //Not allowed
+
+
+
+accountEmail = "ab123@asd.in"
+accountPassword = "1234"
+accountCity = "Faizabad"
+
+// console.log(accountId);
+/*
+prefer not to use var
+because of isse in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
---
+++ datatype.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not brpwser
+
+console.log(3 
+    +
+     3); // code readability should be high
+
+console.log("Nishendra");
+
+
+let name = "Nishendra"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// -- Premitive data type -- 
+// Number => 2 to power 53
+// bigInt
+// String => ""
+// Boolean => true/false
+// Null => The is a Standlone value
+// Undefine => 
+// Symbol unique
+
+// Object
+
+console.log(typeof undefined); // Undefine
+console.log(typeof null); // Object
