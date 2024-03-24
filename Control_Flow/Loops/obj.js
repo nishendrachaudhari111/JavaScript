87cd5deb8c0ff7d8e8f5b736ea567b86062ba1af
Nishendra Chaudhari <149066940+nishendrachaudhari111@users.noreply.github.com>
2024-03-24 10:22:18 +0530

    Object

---
+++ Control_Flow/Loops/obj.js
@@ -0,0 +1,29 @@
+const myobj = {
+    Js: 'JavaScript',
+    Cpp: 'C++',
+    RB: 'Ruby',
+    Swift: 'Swift by Apple'
+}
+
+for (const key in myobj) {
+//   console.log(`${key} shortcut is for ${myobj[key]}`);
+    
+}
+
+const programming = ["js", "rb", "py", "java" ]
+
+for (const key in programming) {
+    // console.log(programming[key]);
+}
+
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('US', "United State of America")
+// map.set('FR', "France")
+// map.set('DU', "Dubai")
+
+// for (const key in map) {
+//     console.log(key);
+        
+//     }
