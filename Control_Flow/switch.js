1826ec000f4d9212380ca2f66b97601e9e3ecb9a
Nishendra Chaudhari <149066940+nishendrachaudhari111@users.noreply.github.com>
2024-03-24 10:24:14 +0530

    Switch and break

---
+++ Control_Flow/switch.js
@@ -0,0 +1,31 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 2
+
+switch (month) {
+    case 1:
+        console.log("Jabuary");
+        break;
+    case 2:
+        console.log("Febuary");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    default:
+        console.log("defualt case match");
+        break;
+}
